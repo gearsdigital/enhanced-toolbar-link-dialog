8d6a86e45d49f53644637e30ecdb4b58a013dffa
Māris <vilskersts.maris@gmail.com>
2020-12-01 22:37:20 +0200

    Added title and anchor bookmarks fields to internal links (#38)
    feat: add title and anchor fields to internal links dialog

--- cypress/integration/dialog_spec.js
+++ cypress/integration/dialog_spec.js
@@ -5,7 +5,7 @@ describe("Enhanced Link Toolbar Dialog", () => {
     cy.openDialog();
     cy.openInternalTab();
 
-    cy.get(".k-text-input").type("jun",{delay: 100}).should("have.value", "jun");
+    cy.get(".k-dialog-search .k-text-input").type("jun",{delay: 500}).should("have.value", "jun");
 
     cy.wait(1000);
 
@@ -22,8 +22,8 @@ describe("Enhanced Link Toolbar Dialog", () => {
 
     cy.get(".k-pages-dialog .k-dialog-pagination")
       .find('button[title="Next"]')
-      .click()
-      .click();
+      .click({force: true})
+      .click({force: true});
 
     cy.get(".k-pages-dialog .k-list")
       .find(".k-list-item")
@@ -67,13 +67,13 @@ describe("Enhanced Link Toolbar Dialog", () => {
 
         cy.get(".k-pages-dialog .k-list")
           .find(".k-list-item:first-child")
-          .click();
+          .click({force: true});
 
-        cy.get(".k-button").contains("Ok").click();
+        cy.get(".k-button").contains("Ok").click({force: true});
 
         cy.get(".k-textarea-field textarea").should(
           "include.value",
-          "(link: /notes/in-the-jungle-of-sumatra text: In the jungle of Sumatra)"
+          "(link: /notes/in-the-jungle-of-sumatra text: ### In the jungle of Sumatra ###)"
         );
       });
 
@@ -91,9 +91,9 @@ describe("Enhanced Link Toolbar Dialog", () => {
 
         cy.get(".k-pages-dialog .k-list")
           .find(".k-list-item:first-child")
-          .click();
+          .click({force: true});
 
-        cy.get(".k-button").contains("Ok").click();
+        cy.get(".k-button").contains("Ok").click({force: true});
 
         cy.get(".k-textarea-field textarea").should(
           "include.value",
@@ -115,14 +115,93 @@ describe("Enhanced Link Toolbar Dialog", () => {
 
         cy.get(".k-pages-dialog .k-list")
           .find(".k-list-item:first-child")
-          .click();
+          .click({force: true});
         cy.get(".k-pages-dialog .k-select-input-native").select("Blank");
 
-        cy.get(".k-button").contains("Ok").click();
+        cy.get(".k-button").contains("Ok").click({force: true});
 
         cy.get(".k-textarea-field textarea").should(
           "include.value",
           "(link: /notes/in-the-jungle-of-sumatra text: Lorem ipsum dolor target: _blank)"
+        );
+      });
+
+      it('should insert "text:" from selection with #anchor', () => {
+        cy.login();
+        cy.openFirstNote();
+
+        cy.get(".k-textarea-field textarea")
+            .clear()
+            .type("Lorem ipsum dolor")
+            .type("{selectall}");
+
+        cy.openDialog();
+        cy.openInternalTab();
+
+        cy.get(".k-pages-dialog .k-list")
+            .find(".k-list-item:first-child")
+            .click({force: true});
+
+        cy.get(".k-pages-dialog .k-select-input-native").select("Blank");
+        cy.get(".k-pages-dialog .k-field-name-anchor .k-text-input").type("anchor");
+
+        cy.get(".k-button").contains("Ok").click({force: true});
+        cy.get(".k-textarea-field textarea").should(
+            "include.value",
+            "(link: /notes/in-the-jungle-of-sumatra#anchor text: Lorem ipsum dolor target: _blank)"
+        );
+      });
+
+      it('should insert "text:" from selection with :title', () => {
+        cy.login();
+        cy.openFirstNote();
+
+        cy.get(".k-textarea-field textarea")
+            .clear()
+            .type("Lorem ipsum dolor")
+            .type("{selectall}");
+
+        cy.openDialog();
+        cy.openInternalTab();
+
+        cy.get(".k-pages-dialog .k-list")
+            .find(".k-list-item:first-child")
+            .click({force: true});
+
+        cy.get(".k-pages-dialog .k-select-input-native").select("Blank");
+        cy.get(".k-pages-dialog .k-field-name-title .k-text-input").type("Title");
+
+        cy.get(".k-button").contains("Ok").click({force: true});
+        cy.get(".k-textarea-field textarea").should(
+            "include.value",
+            "(link: /notes/in-the-jungle-of-sumatra text: Lorem ipsum dolor target: _blank title: Title)"
+        );
+      });
+
+      it('should insert "text:" from selection with anchor, :target and :title', () => {
+        cy.login();
+        cy.openFirstNote();
+
+        cy.get(".k-textarea-field textarea")
+            .clear()
+            .type("Lorem ipsum dolor")
+            .type("{selectall}");
+
+        cy.openDialog();
+        cy.openInternalTab();
+
+        cy.get(".k-pages-dialog .k-list")
+            .find(".k-list-item:first-child")
+            .click({force: true});
+
+        cy.get(".k-pages-dialog .k-select-input-native").select("Blank");
+        cy.get(".k-pages-dialog .k-field-name-anchor .k-text-input").type("anchor");
+        cy.get(".k-pages-dialog .k-field-name-title .k-text-input").type("Title");
+
+        cy.get(".k-button").contains("Ok").click({force: true});
+        cy.get(".k-textarea-field textarea").should(
+            "include.value",
+            "(link: /notes/in-the-jungle-of-sumatra#anchor text: Lorem ipsum dolor target: _blank title: Title)"
         );
       });
     });
@@ -137,7 +216,7 @@ describe("Enhanced Link Toolbar Dialog", () => {
         cy.openDialog();
         cy.openInternalTab();
 
-        cy.get(".k-button").contains("Ok").click();
+        cy.get(".k-button").contains("Ok").click({force: true});
 
         cy.get(".k-textarea-field textarea").should(
           "include.value",
@@ -157,7 +236,7 @@ describe("Enhanced Link Toolbar Dialog", () => {
         cy.openDialog();
         cy.openInternalTab();
 
-        cy.get(".k-button").contains("Ok").click();
+        cy.get(".k-button").contains("Ok").click({force: true});
 
         cy.get(".k-textarea-field textarea").should(
           "include.value",
