7e49d012735f2cedf64ec3bae26046226815df0d
gearsdigital <steffen.giers@gmail.com>
2022-08-16 21:23:52 +0200

    fix(localization): add missing localizations and translations

--- translations.php
+++ translations.php
@@ -3,29 +3,38 @@
 return [
     'en' => [
         'gearsdigital.enhanced-toolbar-link-dialog.anchor.title' => 'Anchor',
+        'gearsdigital.enhanced-toolbar-link-dialog.autosuggest.placeholder' => 'Enter a URL or search term…',
     ],
     'de' => [
         'gearsdigital.enhanced-toolbar-link-dialog.anchor.title' => 'Anker',
+        'gearsdigital.enhanced-toolbar-link-dialog.autosuggest.placeholder' => 'URL oder Suchbegriff eingeben…',
     ],
     'cs' => [
         'gearsdigital.enhanced-toolbar-link-dialog.anchor.title' => 'Anchor',
+        'gearsdigital.enhanced-toolbar-link-dialog.autosuggest.placeholder' => 'Zadejte adresu URL nebo hledaný výraz…',
     ],
     'fr' => [
         'gearsdigital.enhanced-toolbar-link-dialog.anchor.title' => 'Ancre',
+        'gearsdigital.enhanced-toolbar-link-dialog.autosuggest.placeholder' => 'Entrez une URL ou un terme de recherche…',
     ],
     'nl' => [
         'gearsdigital.enhanced-toolbar-link-dialog.anchor.title' => 'Anker',
+        'gearsdigital.enhanced-toolbar-link-dialog.autosuggest.placeholder' => 'Voer een URL of zoekterm in…',
     ],
     'is' => [
         'gearsdigital.enhanced-toolbar-link-dialog.anchor.title' => 'Bensli',
+        'gearsdigital.enhanced-toolbar-link-dialog.autosuggest.placeholder' => 'Sláðu inn vefslóð eða leitarorð…',
     ],
     'da' => [
         'gearsdigital.enhanced-toolbar-link-dialog.anchor.title' => 'Anker',
+        'gearsdigital.enhanced-toolbar-link-dialog.autosuggest.placeholder' => 'Indtast en URL eller et søgeord…',
     ],
     'se' => [
         'gearsdigital.enhanced-toolbar-link-dialog.anchor.title' => 'Ankere',
+        'gearsdigital.enhanced-toolbar-link-dialog.autosuggest.placeholder' => 'Ange en webbadress eller sökterm…',
     ],
     'no' => [
         'gearsdigital.enhanced-toolbar-link-dialog.anchor.title' => 'Anker',
+        'gearsdigital.enhanced-toolbar-link-dialog.autosuggest.placeholder' => 'Skriv inn en URL eller et søkeord…',
     ],
 ];
