5979294afc226c7e337de771296ade5f6def58a0
gearsdigital <steffen.giers@gmail.com>
2023-04-14 08:51:47 +0200

    chore: replace deprecated DS constant to ensure compatibility to kirby core
    Closes #83

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 Kirby::plugin('gearsdigital/enhanced-toolbar-link-dialog', [
-    'translations' => require_once __DIR__.DS.'translations.php',
-    'hooks' => require_once __DIR__.DS.'hooks.php',
-    'api' => require_once __DIR__.DS.'routes.php',
+    'translations' => require_once __DIR__.'/translations.php',
+    'hooks' => require_once __DIR__.'/hooks.php',
+    'api' => require_once __DIR__.'/routes.php',
 ]);
